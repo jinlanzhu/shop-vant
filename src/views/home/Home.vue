19805bd80003291cb8f593c31428ce48d8c1a362
Lee <5389233+zhu_jin_lan@user.noreply.gitee.com>
2020-09-06 17:00:53 +0800

    完成项目的基本配置

---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>我是主页</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped>
+</style>
